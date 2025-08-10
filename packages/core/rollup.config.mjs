34a91a9ff3679a31564a1ad5eb160e17d0474614
wu9o <wu9ors@gmail.com>
2025-08-10 14:58:18 +0800

    fix: build core

---
+++ packages/core/rollup.config.mjs
@@ -0,0 +1,46 @@
+import esbuild from 'rollup-plugin-esbuild';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
+
+const external = [
+  'react',
+  'react-dom',
+  'react/jsx-runtime',
+  'react-router-dom',
+  '@garfish/browser-vm',
+];
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.mjs',
+        format: 'esm',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      esbuild({
+        jsx: 'automatic',
+        minify: true,
+        target: 'esnext',
+      }),
+    ],
+    external,
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: ['./SandboxMFE'], // Exclude SandboxMFE from the final d.ts bundle
+  },
+];
