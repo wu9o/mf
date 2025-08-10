c000d66c388759993a4b76bff478dc2ab004067c
wu9o <wu9ors@gmail.com>
2025-08-10 15:09:03 +0800

    fix: deploy public

--- examples/main-app/webpack.prod.js
+++ examples/main-app/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/mf/',
+    publicPath: PROD_BASE_PATH,
   },
   plugins: [
     new ModuleFederationPlugin({
